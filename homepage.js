85e3546c7248dc04ef68220cac1510700c197798
Ryan Dahl <ry@tinyclouds.org>
2020-12-16 13:04:48 -0500

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -16,7 +16,7 @@ homepage(() => {
         src: "https://tinyclouds.org/ry.jpg",
         style: "max-width: 120px",
       }),
-      h("h2", {}, "Ryan Dahl"),
+      h("h2", {}, "Ryan Dahl hallo"),
       h("a", { href: "ryan@deno.com" }, "ryan@deno.com")
     ),
     h(
