f6cd400e76ebb68119d06cfcca14aff912afa141
Ryan Dahl <ry@tinyclouds.org>
2021-01-13 16:26:13 -0500

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -16,7 +16,7 @@ homepage(() => {
         src: "https://tinyclouds.org/ry.jpg",
         style: "max-width: 120px",
       }),
-      h("h2", {}, "Dan"),
+      h("h2", {}, "Lee hello"),
       h("a", { href: "ryan@deno.com" }, "ryan@deno.com")
     ),
     h(
